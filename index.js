62bf86380e1940d1a76f88b0b580e736386a0474
trangtran <tranthutrangcppp@gmail.com>
2025-06-21 20:25:14 +0900

    render

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ import reportWebVitals from './reportWebVitals';
 import Library from './chapter_03/Library';
 import Clock from './chapter_04/Clock';
 import CommentList from './chapter_05/CommentList';
+import NotificationList from './chapter_06/NotificationList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-setInterval(() => {
   root.render(
     <React.StrictMode>
-      <CommentList />
+      <NotificationList />
     </React.StrictMode>
   );
-}, 1000);
+
 
 
 // If you want to start measuring performance in your app, pass a function
